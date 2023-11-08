670da9e5df604da0784e9fb61584dd53ef0c1385
minfrank1 <minfrank@naver.com>
2023-11-09 03:13:36 +0900

    null 처리

--- app/src/main/java/org/techtown/cap2/view/DrinkPageboomActivity.java
+++ app/src/main/java/org/techtown/cap2/view/DrinkPageboomActivity.java
@@ -294,6 +294,16 @@ public class DrinkPageboomActivity extends AppCompatActivity {
         btn=findViewById(R.id.roul);
         btn.setOnClickListener(v -> {
 
+            if(num1 == null) {
+                num1 = "0";
+            }
+            if(num2 == null) {
+                num2 = "0";
+            }
+            if(num3 == null) {
+                num3 = "0";
+            }
+
             sendDataToBluetooth(num1,num2,num3);
             Log.d("TAG", "전송된 데이터: " + num1);
             Log.d("TAG", "전송된 데이터: " + num2);
